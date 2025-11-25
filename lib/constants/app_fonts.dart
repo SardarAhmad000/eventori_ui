49846d45bc3deb945b51878b3bb199682386dd35
SardarAhmad000 <ahmadshahzad01434@gmail.com>
2025-11-25 20:46:35 +0500

    Update App Fonts

--- lib/constants/app_fonts.dart
+++ lib/constants/app_fonts.dart
@@ -1,9 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
-import '../AppTheme/app_theme.dart';
-
-
 class AppFonts{
   static const bold = 'bold'; // use regular
   // static const light = 'bold';
@@ -12,15 +6,4 @@ class AppFonts{
 
 //FontFamily
   static const String fontFamily = "Outfit";
-
-  // Text Styles
-  static TextStyle StylePara({double fontSize = 14, Color? color,FontWeight fontWeight = FontWeight.w400,}) {
-    return TextStyle(
-      fontFamily: fontFamily,
-      fontSize: fontSize,
-      color: color ?? AppTheme.whiteColor,
-      fontWeight: fontWeight,
-    );
-  }
-
 }
